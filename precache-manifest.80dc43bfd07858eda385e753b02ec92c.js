c02b16a9d3f0a02704e5a1b776753eb51685ff85
Abhishek Masurkar <abhishek.mas@directi.com>
2019-07-20 07:57:45 +0530

    Updates

---
+++ precache-manifest.80dc43bfd07858eda385e753b02ec92c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5a226315fcd22cbb08110dd2d2dba4",
+    "url": "/police-neigbourhood-explorer/index.html"
+  },
+  {
+    "revision": "de908eb9d424e8de02de",
+    "url": "/police-neigbourhood-explorer/static/css/2.7fad5d03.chunk.css"
+  },
+  {
+    "revision": "c0c57e6ce78a602e1e2b",
+    "url": "/police-neigbourhood-explorer/static/css/main.2928c90d.chunk.css"
+  },
+  {
+    "revision": "de908eb9d424e8de02de",
+    "url": "/police-neigbourhood-explorer/static/js/2.7437546e.chunk.js"
+  },
+  {
+    "revision": "c0c57e6ce78a602e1e2b",
+    "url": "/police-neigbourhood-explorer/static/js/main.1ea76376.chunk.js"
+  },
+  {
+    "revision": "e42a5efb30c1a0bc7fb7",
+    "url": "/police-neigbourhood-explorer/static/js/runtime~main.13cc8161.js"
+  },
+  {
+    "revision": "ce6d299b64125d8a252d987ccef7bc0b",
+    "url": "/police-neigbourhood-explorer/static/media/main-banner.ce6d299b.jpg"
+  },
+  {
+    "revision": "f62d0a7530d75eb9bb4fc32007186273",
+    "url": "/police-neigbourhood-explorer/static/media/main-logo.f62d0a75.svg"
+  },
+  {
+    "revision": "6db902d371583589e5a17932195c9a4c",
+    "url": "/police-neigbourhood-explorer/static/media/no-events.6db902d3.png"
+  }
+]);
