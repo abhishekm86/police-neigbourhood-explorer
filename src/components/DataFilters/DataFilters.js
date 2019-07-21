1bdeef8792293d60405245b09367d33320087374
Abhishek Masurkar <abhishek.mas@directi.com>
2019-07-21 16:10:03 +0530

    bound filter control states

--- src/components/DataFilters/DataFilters.js
+++ src/components/DataFilters/DataFilters.js
@@ -15,6 +15,7 @@ class DataFilters extends Component{
 		};
 		this.handleChange = this.handleChange.bind(this);
 		this.applyFilters = this.applyFilters.bind(this);
+		this.resetFilterSelections = this.resetFilterSelections.bind(this);
 	}
 	handleChange( evt ){
 		if(evt.target.name === 'filterStartDate' && evt.target.value.length > 0 )
@@ -26,15 +27,29 @@ class DataFilters extends Component{
 			[evt.target.name] : evt.target.value
 		});
 	}
+	resetFilterSelections( evt ){
+		evt.preventDefault();
+		const { filters } = this.props;
+		this.setState({
+			filterByType: (filters.filterByType) ? filters.filterByType : '',
+			filterByDate: (filters.filterByDate) ? filters.filterByDate : '',
+			filterStartDate: (filters.filterStartDate) ? filters.filterStartDate : '',
+			filterEndDate: (filters.filterEndDate) ? filters.filterEndDate : '',
+			errorStartDate: false,
+			errorEndDate: false,
+			errorIncorrectEndDate: false
+		})
+		this.props.closeFilterHandler();
+	}
 	applyFilters( evt ) {
 		evt.preventDefault();
 		const { filterByDate, filterEndDate, filterStartDate } = this.state;
 		if( ( filterByDate === 'custom' ) && ( filterStartDate.length === 0 || filterEndDate.length === 0 || Moment(filterEndDate).diff(Moment(filterStartDate), 'days') < 0) ) {
-			if( filterStartDate.length === 0 ) 
+			if( filterStartDate.length === 0 )
 				this.setState({errorStartDate : true})
 			else
 				this.setState({errorStartDate : false});
-			if( filterEndDate.length === 0 ) 
+			if( filterEndDate.length === 0 )
 				this.setState({errorEndDate : true});
 			else
 				this.setState({errorEndDate : false});
@@ -55,7 +70,7 @@ class DataFilters extends Component{
 		}
 	}
 	render(){
-		const { closeFilterHandler, isOpen, typeFilters } = this.props;
+		const { isOpen, typeFilters } = this.props;
 		const { errorStartDate, errorEndDate, errorIncorrectEndDate } = this.state;
 		return (
 			<section className= { !isOpen ? 'DataFilters App__hidden' : 'DataFilters' } >
@@ -70,14 +85,7 @@ class DataFilters extends Component{
 											typeFilters.map( (type, index) =>
 												<li key={index+type} className='m-b-15'>
 													<label>
-														<input
-															className='m-r-5'
-															type="radio"
-															name="filterByType"
-															value={ type }
-															checked= { this.state.filterByType === type}
-															onChange = { this.handleChange }
-														/> {type}
+														<input className='m-r-5' type="radio" name="filterByType" value={ type } checked= { this.state.filterByType === type} onChange = { this.handleChange } /> { type }
 													</label>
 												</li>
 											)
@@ -91,87 +99,48 @@ class DataFilters extends Component{
 							<ul className="DataFilters__choices">
 								<li className='m-b-15'>
 									<label>
-										<input
-											type="radio"
-											name="filterByDate"
-											value='today'
-											onChange = { this.handleChange }
-										/> Today
+										<input type="radio" name="filterByDate" value='today' onChange = { this.handleChange } checked= { this.state.filterByDate === 'today' } /> Today
 									</label>
 								</li>
 								<li className='m-b-15'>
 									<label>
-										<input
-											type="radio"
-											name="filterByDate"
-											value='week'
-											onChange = { this.handleChange }
-										/> Next 7 Days
+										<input type="radio" name="filterByDate" value='week' onChange = { this.handleChange } checked= { this.state.filterByDate === 'week' } /> Next 7 Days
 									</label>
 								</li>
 								<li className='m-b-15'>
 									<label>
-										<input
-											type="radio"
-											name="filterByDate"
-											value='fortnight'
-											onChange = { this.handleChange }
-										/> Next 15 Days
+										<input type="radio" name="filterByDate" value='fortnight' onChange = { this.handleChange } checked= { this.state.filterByDate === 'fortnight' } /> Next 15 Days
 									</label>
 								</li>
 								<li className='m-b-15'>
 									<label>
-										<input
-											type="radio"
-											name="filterByDate"
-											value='month'
-											onChange = { this.handleChange }
-										/> Next 30 Days
+										<input type="radio" name="filterByDate" value='month' onChange = { this.handleChange } checked= { this.state.filterByDate === 'month' } /> Next 30 Days
 									</label>
 								</li>
 								<li className='m-b-15'>
 									<label className='m-b-15'>
-										<input
-											type="radio"
-											name="filterByDate"
-											value='custom'
-											onChange = { this.handleChange }
-										/> Custom Date Range
+										<input type="radio" name="filterByDate" value='custom' onChange = { this.handleChange } checked= { this.state.filterByDate === 'custom' } /> Custom Date Range
 									</label>
 									<div className={ this.state.filterByDate !== 'custom' ? "DataFilters__datewrap App__hidden flexbox flex-d-column p-l-25" : "DataFilters__datewrap flexbox flex-d-column p-l-25" } >
 										<label className="flexbox m-b-10">
 											<span className="DataFilters__datewraplabel m-r-5">Start Date: </span>
-											<input
-												type="date"
-												name="filterStartDate"
-												placeholder="Choose a start date"
-												onChange = { this.handleChange }
-											 />
+											<input type="date" name="filterStartDate" placeholder="Choose a start date" value={this.state.filterStartDate} onChange = { this.handleChange } />
 										</label>
 										{ errorStartDate ? <span className="App__errormsg">Please choose a start date!</span> : null }
 										<label className="flexbox m-b-10">
 											<span className="DataFilters__datewraplabel m-r-5">End Date: </span>
-											<input
-												type="date"
-												name="filterEndDate"
-												placeholder="Choose an end date"
-												onChange = { this.handleChange }
-											/>
+											<input type="date" name="filterEndDate" placeholder="Choose an end date" value={this.state.filterEndDate} onChange = { this.handleChange } />
 										</label>
 										{ errorEndDate ? <span className="App__errormsg">Please choose an end date!</span> : null }
-										{ errorIncorrectEndDate ? <span className="App__errormsg">Start date cannot be greater than end date!</span> : null }
+										{ errorIncorrectEndDate ? <span className="App__errormsg">End date cannot be before the start date!</span> : null }
 									</div>
 								</li>
 							</ul>
 						</div>
 					</div>
 					<hr className='m-t-15 m-b-30' />
-					<button
-						className="App__btn App__btn--primary"
-						type="Submit"
-						onClick = {this.applyFilters}
-					>Apply Filters</button>
-					<button onClick={ closeFilterHandler } className="App__btn">Close</button>
+					<button className="App__btn App__btn--primary" type="Submit" onClick = {this.applyFilters} >Apply Filters</button>
+					<button onClick={ this.resetFilterSelections } className="App__btn">Close</button>
 				</div>
 			</section>
 		);
